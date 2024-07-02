b6520257573ff80f65206425983984ad9503bd42
Mishkat <mishkat2008@gmail.com>
2024-07-02 15:36:25 +0200

    modified images for 3 class and grayscale 3 class

--- potato_good_malformed/convert_grayscale.py
+++ potato_good_malformed/convert_grayscale.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import pathlib
 
-for p in pathlib.Path('/home/mishkat/Downloads/potato_good_malformed/malformed_potatoes_fourier_2_class_gray/malformed').iterdir():
+for p in pathlib.Path('/home/mishkat/Downloads/malformed_potato/potato_good_malformed/malformed_potatoes_fourier_3_class_gray/ugly').iterdir():
     img = Image.open(p)
     img = img.convert('L')
     img.save(p)
